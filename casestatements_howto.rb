3ebd10abf6ce0e99304f86ad4ffb1476ba989753
Cristal Alatorre Perez <cristalcodes@gmail.com>
2019-10-29 21:50:52 +0000

    added cli, dogs, and scrape files

---
+++ casestatements_howto.rb
@@ -0,0 +1,23 @@
+#EXAMPLE 1
+#greeting= "friendly_greeting"
+
+#case greeting
+#  when "unfriendly_greeting"
+#    puts "What do you want!?"
+#  when "friendly_greeting"
+#    puts "Hi! How are you?"
+#end 
+
+
+#EXAMPLE 2
+
+current_weather = "raining"
+
+case current_weather
+  when "sunny"
+    puts "grab some sunscreen!"
+  when "raining"
+    puts "grab an umbrella"
+  when "snowing"
+    puts "bundle up"
+end
